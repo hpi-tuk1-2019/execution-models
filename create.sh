7f5a8c3f5b8fb3e9ae01b61ed64a9fcd692c0832
Marius Danner <mariusdanner@Mariuss-MacBook-Pro.local>
2019-04-29 14:55:11 +0200

    Add explaining comments to create.sh

--- create.sh
+++ create.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+#create header, source and test files for a given name
+# bash create.sh ClassName
+
 cp src/Template.hpp src/$1.hpp
 sed -i '' "s/Template/$1/g" src/$1.hpp
 cp src/Template.cpp src/$1.cpp
