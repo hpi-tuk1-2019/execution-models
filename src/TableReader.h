883d4fc59f89f6ef9af4b5138eb85370a24ca548
Marius Danner <mariusdanner@Mariuss-MacBook-Pro.local>
2019-04-29 15:23:32 +0200

    Add all rows of the LINEITEM table

--- src/TableReader.h
+++ src/TableReader.h
@@ -1,8 +1,22 @@
 #include <vector>
 
 struct table {
-    std::vector<int> col1;
-    std::vector<std::array<char, 20>> col2;
+    std::vector<int> l_orderkey; // id
+    std::vector<int> l_partkey; // id
+    std::vector<int> l_suppkey; // id
+    std::vector<int> l_linenumber; // integer
+    std::vector<int> l_quantity; // decimal
+    std::vector<int> l_extendedprice; // decimal
+    std::vector<int> l_discount; // decimal
+    std::vector<int> l_tax; // decimal
+    std::vector<char> l_returnflag; // fixed text, size 1
+    std::vector<char> l_linestatus; // fixed text, size 1
+    std::vector<int> l_shipdate; // date
+    std::vector<int> l_commitdate; // date
+    std::vector<int> l_receiptdate; // date
+    std::vector<dbString> l_shipinstruct; // fixed text, size 25
+    std::vector<dbString> l_shipmode; // fixed text, size 10
+    std::vector<dbString> l_comment; // variable text size 44
 };
 
 table readFile(std::string filename);
