f8df3f41e980af2d960ec5b75349af7cfb48afb0
Lukas Rambold <lukas@rambold.io>
2019-05-06 17:23:41 +0200

    added sample size as parameter

--- src/Main.cpp
+++ src/Main.cpp
@@ -9,8 +9,7 @@ void print_char_array(std::array<char, 45> char_array) {
 	std::cout <<  ", ";
 }
 
-void print_sample(table table_obj) {
-    int sample_size = 20;
+void print_sample(table table_obj, int sample_size = 20) {
     for (int i = 0; i < sample_size; i++){
         std::cout << table_obj.l_orderkey.at(i) << ", ";
         std::cout << table_obj.l_partkey.at(i) << ", ";
