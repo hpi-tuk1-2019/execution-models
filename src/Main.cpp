e7f548de970c36fdfdba32181e9d209fe3186b45
julkw <juliane.waack@gmail.com>
2019-05-06 16:41:32 +0200

    Implement Scan (<) over int columns

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,11 +1,13 @@
-#include "TableReader.h"
+#include "Scan.h"
 #include <string>
 
 int main(int argc, char *argv[]) {
     std::string filename = "../../assets/sample_data/lineitem.tbl";
     char delim = '|';
 
-    auto table = readFile(filename, delim);
+    table fileTable = readFile(filename, delim);
+
+    auto newTable = filterValuesSmaller(fileTable, fileTable.l_receiptdate, 1996);
 
     return 0;
 }
