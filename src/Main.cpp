dbf2bfd5ba4d96032bfbd6e3081260dc6f29fd60
Lukas Rambold <lukas@rambold.io>
2019-05-06 17:19:51 +0200

    works (with char array print out !!)

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,27 +2,37 @@
 #include <string>
 #include <iostream>
 
+void print_char_array(std::array<char, 45> char_array) {
+	for (int i = 0; i < sizeof(char_array); i++) {
+		std::cout << char_array[i];
+	}
+	std::cout <<  ", ";
+}
+
 void print_sample(table table_obj) {
     int sample_size = 20;
     for (int i = 0; i < sample_size; i++){
-        // std::cout << table_obj.l_orderkey.at(i) << std::endl;
-        // std::cout << table_obj.l_partkey.at(i) << std::endl;
-        // std::cout << table_obj.l_suppkey.at(i) << std::endl;
-        // std::cout << table_obj.l_linenumber.at(i) << std::endl;
-        std::cout << table_obj.l_quantity.at(i) << std::endl;
-        // std::cout << table_obj.l_extendedprice.at(i) << std::endl;
-        // std::cout << table_obj.l_discount.at(i) << std::endl;
-        // std::cout << table_obj.l_tax.at(i) << std::endl;
-        // std::cout << table_obj.l_returnflag.at(i) << std::endl;
-        // std::cout << table_obj.l_linestatus.at(i) << std::endl;
-        std::cout << table_obj.l_shipdate.at(i) << std::endl;
-        // std::cout << table_obj.l_commitdate.at(i) << std::endl;
-        // std::cout << table_obj.l_receiptdate.at(i) << std::endl;
-        // std::cout << table_obj.l_shipinstruct.at(i) << std::endl;
-        // std::cout << table_obj.l_shipmode.at(i) << std::endl;
-        // std::cout << table_obj.l_comment.at(i) << std::endl;
+        std::cout << table_obj.l_orderkey.at(i) << ", ";
+        std::cout << table_obj.l_partkey.at(i) << ", ";
+        std::cout << table_obj.l_suppkey.at(i) << ", ";
+        std::cout << table_obj.l_linenumber.at(i) << ", ";
+        std::cout << table_obj.l_quantity.at(i) << ", ";
+        std::cout << table_obj.l_extendedprice.at(i) << ", ";
+        std::cout << table_obj.l_discount.at(i) << ", ";
+        std::cout << table_obj.l_tax.at(i) << ", ";
+        std::cout << table_obj.l_returnflag.at(i) << ", ";
+        std::cout << table_obj.l_linestatus.at(i) << ", ";
+        std::cout << table_obj.l_shipdate.at(i) << ", ";
+        std::cout << table_obj.l_commitdate.at(i) << ", ";
+        std::cout << table_obj.l_receiptdate.at(i) << ", ";
+
+        print_char_array(table_obj.l_shipinstruct.at(i));
+        print_char_array(table_obj.l_shipmode.at(i));
+        print_char_array(table_obj.l_comment.at(i));
+        std::cout << "\n";
     }
 }
+
 
 int main(int argc, char *argv[]) {
     std::string filename = "../../assets/sample_data/lineitem.tbl";
