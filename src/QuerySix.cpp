27ebb4c3548b59dada5d2caec88d843ee476ba6a
MariusDanner <marius@danner.online>
2019-05-10 14:55:57 +0200

    Fix indices bug

--- src/QuerySix.cpp
+++ src/QuerySix.cpp
@@ -17,7 +17,7 @@ double QuerySix::execute(table *tab){
 double QuerySix::op_agg_sum(table *tab, std::vector<int> old_inds){
   double sum = 0;
   for (int i = 0; i < old_inds.size(); i++) {
-    sum += tab->l_extendedprice[i] * tab->l_discount[i];
+    sum += tab->l_extendedprice[old_inds[i]] * tab->l_discount[old_inds[i]];
   }
   return (double)sum / 10000.0;
 
@@ -26,8 +26,8 @@ double QuerySix::op_agg_sum(table *tab, std::vector<int> old_inds){
 std::vector<int> QuerySix::op_shipdate_ge(table *tab, std::vector<int> old_inds){
   std::vector<int> indices;
   for (int i = 0; i < old_inds.size(); i++) {
-    if (tab->l_shipdate[i] >= 757382400) {
-      indices.push_back(i);
+    if (tab->l_shipdate[old_inds[i]] >= 757382400) {
+      indices.push_back(old_inds[i]);
     }
   }
   return indices;
@@ -36,8 +36,8 @@ std::vector<int> QuerySix::op_shipdate_ge(table *tab, std::vector<int> old_inds)
 std::vector<int> QuerySix::op_shipdate_s(table *tab, std::vector<int> old_inds){
   std::vector<int> indices;
   for (int i = 0; i < old_inds.size(); i++) {
-    if (tab->l_shipdate[i] < 788918400) {
-      indices.push_back(i);
+    if (tab->l_shipdate[old_inds[i]] < 788918400) {
+      indices.push_back(old_inds[i]);
     }
   }
   return indices;
@@ -46,8 +46,8 @@ std::vector<int> QuerySix::op_shipdate_s(table *tab, std::vector<int> old_inds){
 std::vector<int> QuerySix::op_quantity_s(table *tab, std::vector<int> old_inds){
   std::vector<int> indices;
   for (int i = 0; i < old_inds.size(); i++) {
-    if (tab->l_quantity[i] < 24) {
-      indices.push_back(i);
+    if (tab->l_quantity[old_inds[i]] < 2400) {
+      indices.push_back(old_inds[i]);
     }
   }
   return indices;
@@ -56,8 +56,8 @@ std::vector<int> QuerySix::op_quantity_s(table *tab, std::vector<int> old_inds){
 std::vector<int> QuerySix::op_discount_between(table *tab, std::vector<int> old_inds){
   std::vector<int> indices;
   for (int i = 0; i < old_inds.size(); i++) {
-    if ((tab->l_orderkey[i] >= 5) && (tab->l_orderkey[i]) <= 5) {
-      indices.push_back(i);
+    if ((tab->l_discount[old_inds[i]] >= 5) && (tab->l_discount[old_inds[i]]) <= 7) {
+      indices.push_back(old_inds[i]);
     }
   }
   return indices;
