424cfcac1f3b47b5208fc7c3e31f32a941abeb31
MariusDanner <marius@danner.online>
2019-05-13 18:42:43 +0200

    Split between function into greater equal and smaller equal

--- src/QuerySix.h
+++ src/QuerySix.h
@@ -6,7 +6,8 @@ class QuerySix {
     double op_agg_sum(const table& tab, std::vector<int> old_inds);
     std::vector<int> op_shipdate_s(const table& tab, std::vector<int> old_inds);
     std::vector<int> op_quantity_s(const table& tab, std::vector<int> old_inds);
-    std::vector<int> op_discount_between(const table& tab, std::vector<int> old_inds);
+    std::vector<int> op_discount_ge(const table& tab, std::vector<int> old_inds);
+    std::vector<int> op_discount_se(const table& tab, std::vector<int> old_inds);
   public:
     double execute(const table& tab);
 };
