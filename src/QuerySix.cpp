ac7d05ecfbdd74829adbdbeccca6e299ba0b0925
MariusDanner <marius@danner.online>
2019-05-28 16:40:01 +0200

    Make QuerySix compiled branch free

--- src/QuerySix.cpp
+++ src/QuerySix.cpp
@@ -6,13 +6,11 @@
 double QuerySix::execute_compiled(const table& tab){
   int sum = 0;
   for (int i = 0; i < tab.l_extendedprice.size(); i++) {
-    if ((tab.l_shipdate[i] >= 757382400) &&
+    sum += (int)((tab.l_shipdate[i] >= 757382400) &&
         (tab.l_shipdate[i] < 788918400) &&
         (tab.l_quantity[i] < 2400) &&
         (tab.l_discount[i] >= 5) &&
-        (tab.l_discount[i] <= 7)) {
-          sum += tab.l_extendedprice[i] * tab.l_discount[i];
-        }
+        (tab.l_discount[i] <= 7)) * tab.l_extendedprice[i] * tab.l_discount[i];
   }
   return (double)sum / 10000.0;
 }
