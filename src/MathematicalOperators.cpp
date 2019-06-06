87c020a94c7da557763e6db227cae79b9395b0da
MariusDanner <marius@danner.online>
2019-06-06 17:17:47 +0200

    Add mathematical operators

---
+++ src/MathematicalOperators.cpp
@@ -0,0 +1,27 @@
+int __attribute__ ((noinline)) db_plus(int a, int b){
+  return a + b;
+}
+int __attribute__ ((noinline)) db_minus(int a, int b){
+  return a - b;
+}
+int __attribute__ ((noinline)) db_times(int a, int b){
+  return a * b;
+}
+int __attribute__ ((noinline)) db_divided(int a, int b){
+  return a / b;
+}
+double __attribute__ ((noinline)) db_minus(double a, double b){
+  return a - b;
+}
+double __attribute__ ((noinline)) db_times(double a, double b){
+  return a * b;
+}
+double __attribute__ ((noinline)) db_divided(double a, double b){
+  return a / b;
+}
+double __attribute__ ((noinline)) db_times(int a, double b){
+  return a * b;
+}
+double __attribute__ ((noinline)) db_times(double a, int b){
+  return a * b;
+}
