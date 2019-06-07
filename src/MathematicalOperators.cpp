249ef50c26abea2fb1c8ea1c8376665610371fd0
MariusDanner <marius@danner.online>
2019-06-07 20:06:46 +0200

    Add support for windows

--- src/MathematicalOperators.cpp
+++ src/MathematicalOperators.cpp
@@ -1,3 +1,33 @@
+#ifdef _WIN32
+__declspec(noinline) int db_plus(int a, int b){
+  return a + b;
+}
+__declspec(noinline) int db_minus(int a, int b){
+  return a - b;
+}
+__declspec(noinline) int db_times(int a, int b){
+  return a * b;
+}
+__declspec(noinline) int db_divided(int a, int b){
+  return a / b;
+}
+__declspec(noinline) double db_minus(double a, double b){
+  return a - b;
+}
+__declspec(noinline) double db_times(double a, double b){
+  return a * b;
+}
+__declspec(noinline) double db_divided(double a, double b){
+  return a / b;
+}
+__declspec(noinline) double db_times(int a, double b){
+  return a * b;
+}
+__declspec(noinline) double db_times(double a, int b){
+  return a * b;
+}
+#elif defined(__unix__) || defined(__APPLE__)
+
 int __attribute__ ((noinline)) db_plus(int a, int b){
   return a + b;
 }
@@ -25,3 +55,4 @@ double __attribute__ ((noinline)) db_times(int a, double b){
 double __attribute__ ((noinline)) db_times(double a, int b){
   return a * b;
 }
+#endif
