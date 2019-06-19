8eafbfe4452ed1cef1903c7f0090daa016d2fd3a
Lukas Rambold <lukas@rambold.io>
2019-06-20 00:00:46 +0200

    corrected use of intel compiler

--- activate_intel.sh
+++ activate_intel.sh
@@ -4,5 +4,5 @@
 export COMPILERVARS_ARCHITECTURE=intel64
 export COMPILERVARS_PLATFORM=linux
 source /opt/intel/compilers_and_libraries_2019.4.243/linux/bin/compilervars.sh
-export CMAKE_CXX_COMPILER=/opt/intel/compilers_and_libraries_2019.4.243/linux/bin/intel64/icc
-export CMAKE_C_COMPILER=/opt/intel/compilers_and_libraries_2019.4.243/linux/bin/intel64/icc
+export CXX=/opt/intel/compilers_and_libraries_2019.4.243/linux/bin/intel64/icc
+export CC=/opt/intel/compilers_and_libraries_2019.4.243/linux/bin/intel64/icc
